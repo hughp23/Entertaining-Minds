4204fcef3ee368b736a3b82b8dd728e13f36c837
hughp23 <hughpaul14@gmail.com>
2020-02-20 13:33:21 +0000

    initial setup of meet the team page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import "./App.css";
 import { Router } from "@reach/router";
 import Homepage from "./components/Homepage";
-import WorkForUs from "./components/WorkForUs";
-import WorkWithUs from "./components/WorkWithUs";
+import WorkForUs from "./components/TheTeam/WorkForUs";
+import WorkWithUs from "./components/Clients/WorkWithUs";
 import Footer from "./components/Footer";
+import MeetTheTeam from "./components/TheTeam/MeetTheTeam";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Homepage path="/" />
-        <WorkForUs path="/workforus" />
+        <WorkForUs path="/theteam/workforus" />
         <WorkWithUs path="/workwithus" />
+        <MeetTheTeam path="/theteam/meettheteam" />
       </Router>
       <Footer path="contact" />
     </div>
